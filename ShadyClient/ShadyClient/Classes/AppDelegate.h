57a826aaa922b5026f090554c6b5d902b3953b87
Dan McCulley <dan.mcculley@gmail.com>
2011-05-22 10:46:52 -0600

    client

---
+++ ShadyClient/ShadyClient/Classes/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  ShadyClient
+//
+//  Created by Dan McCulley on 5/22/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#ifdef PHONEGAP_FRAMEWORK
+	#import <PhoneGap/PhoneGapDelegate.h>
+#else
+	#import "PhoneGapDelegate.h"
+#endif
+
+@interface AppDelegate : PhoneGapDelegate {
+}
+
+@end
+
---
+++ ShadyClient/ShadyClient/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ShadyClient
+//
+//  Created by Dan McCulley on 5/22/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
